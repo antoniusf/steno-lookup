49c28716fd7b04744b3ff189b3331a5941d92730
Antonius Frie <antonius.frie@ruhr-uni-bochum.de>
2020-03-25 17:39:59 +0100

    Use postMessage instead of setTimeout

--- src/ReverseLookup.svelte
+++ src/ReverseLookup.svelte
@@ -7,13 +7,46 @@
 	chunk: 0,
 	query: null
     };
-    const itemsPerChunk = 1000;
+    const itemsPerChunk = 10000;
 
     // perf
     let startQueryTime = null;
 
     $: startQuery(query)
 
+    // I feel like I owe you a quick explanation of what is going on
+    // here. What you have to know about this app is that my development
+    // target is my 8 year old Galaxy S2, and I want the app to run well
+    // on that.
+    //
+    // If I just do the simple thing and let the whole query run through
+    // each time the input changes, it slows down the UI noticably when
+    // keys are pressed in quick succession (again, on my phone). This is
+    // because, though quite fast, it still takes a small amount of time
+    // to search through all entries at once, and since this happens every
+    // time you press a key, it can add up. Now the obvious solution might
+    // be to add a web worker, but remember that I am working with a
+    // really slow phone and want to avoid as much overhead as possible.
+    //
+    // So the solution that I chose was chunking the query into subqueries
+    // that are small enough they don't block the UI, and return control
+    // to the event loop in between chunks, so it can process any new
+    // events. If we get a change event while the old query is still
+    // going, we simply abort it and start running the new query! This is
+    // basically cooperative multi-tasking on the main thread. I like this
+    // solution because it has low technical complexity, no background
+    // jobs that can crash my phone, and it's fast!
+    //
+    // My first idea for this was the simple setTimeout with a timeout
+    // value of 0, since that does exactly what we need: queue up our
+    // function on the (macro) task queue, ie. the main event
+    // loop. However, as MDN helpfully tells us, the timeout function is
+    // throttled on modern browsers, meaning that it spends lots of time
+    // doing nothing before querying the next chunk. (See:
+    // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout#Timeouts_throttled_to_%E2%89%A5_4ms)
+    // However, MDN also has the solution: Register your function as an
+    // onMessage handler on the window, and use postMessage to enqueue
+    // it. So that's what I'm doing.
     function startQuery(query) {
 	queryRunnerState.chunk = 0;
 	queryRunnerState.query = query;
@@ -23,14 +56,20 @@
 	    // enqueue the query runner task
 	    // (if running was true, there would already be a task enqueued, which will just start again
 	    // now that we've reset it's state)
-	    window.setTimeout(doQueryInChunks, 0);
+	    window.postMessage("wakeQueryRunner", "*");
             queryRunnerState.running = true;
 	}
 
 	startQueryTime = performance.now();
     }
 
-    function doQueryInChunks() {
+    function doQueryInChunks(event) {
+	if (event.source == window && event.data == "wakeQueryRunner") {
+	    event.stopPropagation();
+	} else {
+	    return;
+	}
+
 	// console.log("query \"" + queryRunnerState.query + "\", chunk " + queryRunnerState.chunk);
 	let startIndex = queryRunnerState.chunk * itemsPerChunk;
 	let endIndex = startIndex + itemsPerChunk;
@@ -56,7 +95,7 @@
 	} else {
 	    // re-enqueue this function at the back of the message queue.
 	    // if there were input events in the meantime, they will get handled first.
-	    window.setTimeout(doQueryInChunks, 0);
+	    window.postMessage("wakeQueryRunner", "*");
 	}
     }
 
@@ -64,6 +103,9 @@
     //     query_result = dictionary.filter(([strokes, translation]) => (translation == query));
     // }
 </script>
+
+<!--Register our query runner in the window's message handler. There's probably a more elegant way to do this so it doesn't risk interfering with anything else, but for now this should work.-->
+<svelte:window on:message={doQueryInChunks}/>
 
 <style>
     table {
